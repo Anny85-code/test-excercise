243c2e2e6195f8443db2bb850a6779d299460ed2
uae <aniekaudo@yahoo.com>
2022-01-19 20:42:50 +0100

    Update js files

--- src/task1.test.js
+++ src/task1.test.js
@@ -3,7 +3,3 @@ const stringLength = require('./task1.js');
 it('length not enough', () => {
   expect(stringLength('Aniekan')).toBe(7);
 });
-
-// if (stringLength('Aniekan') === 1 && stringLength('Aniekan') < 10) {
-//   throw new Error('Check fail:length not enough');
-// }
